1b209909b21d18da174e7fd029a5836aebd6d455
Kimberly Coy <kimberlycoy@mybluetub.com>
2016-03-26 14:26:13 -0400

    Working array

--- tour.js
+++ tour.js
@@ -24,18 +24,23 @@ Tour.prototype.start = function () {
 Tour.prototype.createContainer = function () {
     $('body')
         .append(`<div class="tour-container">
-        		<div class="tour-content"></div>
-        	</div>
-        	<svg class="tour-arrow" width="200" height="220">
-        	<defs>
-        	    <marker id="markerArrow" markerWidth="13" markerHeight="13" refX="9" refY="6" orient="auto-start-reverse">
-                    <path d="M2,10 L10,6 L2,2" style="fill:none; stroke:#FFF" />
+                <div class="tour-content"></div>
+            </div>
+            <svg class="tour-arrow" width="200" height="220">
+            <defs>
+                <marker id="markerArrow"
+                    viewBox="0 0 10 10"
+                    refX="1" refY="5"
+                    markerWidth="6"
+                    markerHeight="6"
+                    orient="auto">
+                    <path d="M 10 10 L 0 5 L 10 0" style="fill:none; stroke:#FFF;" />
                 </marker>
-        	</defs>
-        	<path d="M10,10 C20,200 90,220 200,218"
-        		style="fill:none; stroke:#FFF; stroke-width:2px; marker-start: url(#markerArrow);"/>
-			</svg>
-        	`);
+            </defs>
+            <path d="M10,10 C20,200 90,220 200,218"
+                style="fill:none; stroke:#FFF; stroke-width:2px; marker-start: url(#markerArrow);"/>
+            </svg>
+            `);
 };
 
 Tour.prototype.createOverlay = function () {
